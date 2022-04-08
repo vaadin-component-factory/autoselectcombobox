1dd91bc6778ec7a7ca2c9ecd188dfb2d5237b1c9
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2022-04-08 09:54:45 +0300

    removing unnecessary frontend files

--- src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
+++ src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
 
 @Tag("autoselect-combo-box")
 @JsModule("./autoselect-combo-box.js")
+// @NpmPackage(value = "@vaadin-component-factory/vcf-auto-select-combobox", version="14.8.6")
 public class AutoSelectComboBox<T> extends ComboBox<T> {
 
     public AutoSelectComboBox() {
