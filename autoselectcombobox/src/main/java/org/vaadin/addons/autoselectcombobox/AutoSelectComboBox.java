a9dc2d8a70281e7f4e61540efd55b1d1030cbcad
Paola De Bartolo <paola.debartolo@gmail.com>
2022-07-06 14:36:58 -0300

    build: update client side dependency to 15.0.0

--- autoselectcombobox/src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
+++ autoselectcombobox/src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
@@ -8,7 +8,7 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 
 @Tag("vcf-auto-select-combo-box")
-@NpmPackage(value = "@vaadin-component-factory/vcf-auto-select-combobox", version="14.8.7")
+@NpmPackage(value = "@vaadin-component-factory/vcf-auto-select-combobox", version="15.0.0")
 @JsModule("@vaadin-component-factory/vcf-auto-select-combobox/src/vcf-auto-select-combobox.js")
 public class AutoSelectComboBox<T> extends ComboBox<T> {
 
