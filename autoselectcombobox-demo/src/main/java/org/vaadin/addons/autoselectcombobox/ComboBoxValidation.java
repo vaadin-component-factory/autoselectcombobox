0d5cfb7fbfbf5d282c9d83a82215299ba0af55ab
Olli Tietäväinen <ollit@vaadin.com>
2023-02-02 16:15:40 +0200

    refactor to allow running webpack

---
+++ autoselectcombobox-demo/src/main/java/org/vaadin/addons/autoselectcombobox/ComboBoxValidation.java
@@ -0,0 +1,88 @@
+package org.vaadin.addons.autoselectcombobox;
+
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.Validator;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+
+@PageTitle("ComboBox validation")
+@Route("")
+public class ComboBoxValidation extends AbstractDemo {
+
+    private PersonService personService;
+
+    @Override
+    protected void initView() {
+        personService = new PersonService(1);
+        addComboValidation();
+    }
+
+    private void addComboValidation() {
+        DataProvider<Person, String> dataProvider = DataProvider.fromFilteringCallbacks(
+                query -> personService.fetch(query.getOffset(), query.getLimit(), query.getFilter().orElse(null)).stream(),
+                query -> personService.count(query.getFilter().orElse(null)));
+
+        ComboBox<Person> comboBoxDefault = new ComboBox<>("People");
+        comboBoxDefault.setHelperText("Default behaviour");
+        comboBoxDefault.setDataProvider(dataProvider);
+        comboBoxDefault.setItemLabelGenerator(Person::toString);
+
+        // begin-source-example
+        // source-example-heading: ComboBox with autoselect and validation
+        ComboBox<Person> comboBox = new ComboBox<>("People");
+        comboBox.setHelperText("Auto select if 1 option. Allow custom values + run validation against options.");
+        comboBox.setItemLabelGenerator(Person::toString);
+        comboBox.setDataProvider(dataProvider);
+
+        new ComboBoxEnhancer<>(comboBox).enableAutoSelect(buildEmptyPerson(), (displayValue, emptyValue) -> {
+            emptyValue.setFirstName(displayValue);
+            return emptyValue;
+        }, filter -> personService.fetch(0, Integer.MAX_VALUE, filter).stream());
+
+        AutoSelectComboBox<Person> asComboBox = new AutoSelectComboBox<>("Another People");
+        asComboBox.setHelperText("Custom Web Component. Auto select if 1 option. Allow custom values + run validation against options.");
+        asComboBox.setDataProvider(dataProvider);
+        asComboBox.setItemLabelGenerator(Person::toString);
+        asComboBox.setClearButtonVisible(true);
+        asComboBox.addValueChangeListener(e -> {
+            System.out.println("asComboBox value change to " + e.getValue());
+        });
+        Grid<String> grid = new Grid<>();
+        grid.getDataCommunicator();
+        Binder<TestBean> binder = new Binder<>();
+        binder.forField(comboBox)
+                .asRequired(Validator.from(personService::exists, "Please select one of the available values"))
+                .bind(TestBean::getPerson, TestBean::setPerson);
+
+        TestBean item = new TestBean();
+        binder.setBean(item);
+        // end-source-example
+
+        AutoSelectComboBox<Person> asComboBox2 = new AutoSelectComboBox<>("2 People");
+        asComboBox2.setHelperText("Custom Web Component. Auto select if 1 option. Allow custom values + run validation against options.");
+        asComboBox2.setItems(new Person(1, "first", "last", 22,
+        null, "123"), new Person(2, "firs2t", "l2ast", 32,
+                null, "1223"));
+        asComboBox2.setClearButtonVisible(true);
+        asComboBox2.setItemLabelGenerator(Person::toString);
+        asComboBox2.addValueChangeListener(e -> {
+            System.out.println("asComboBox2 value change to " + e.getValue());
+        });
+
+        addCard("ComboBox with autoselect and validation", comboBoxDefault, comboBox, asComboBox, asComboBox2, new Anchor("#", "Focus target for testing"));
+    }
+
+    private Person buildEmptyPerson() {
+        Person person = new Person();
+        person.setId(-1);
+        person.setFirstName("");
+        person.setLastName("");
+        return person;
+    }
+
+}
---
+++ autoselectcombobox-demo/src/main/java/org/vaadin/addons/autoselectcombobox/TestBean.java
@@ -0,0 +1,25 @@
+package org.vaadin.addons.autoselectcombobox;
+
+public class TestBean {
+
+    private Person person;
+
+    private Person anotherPerson;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Person getAnotherPerson() {
+        return anotherPerson;
+    }
+
+    public void setAnotherPerson(Person anotherPerson) {
+        this.anotherPerson = anotherPerson;
+    }
+
+}
