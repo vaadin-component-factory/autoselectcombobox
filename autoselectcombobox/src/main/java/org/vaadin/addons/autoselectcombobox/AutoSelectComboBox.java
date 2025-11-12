744e05d239b241640efb041dd55ff16a26ba169a
Herberts Markuns <herberts@vaadin.com>
2025-11-12 16:15:20 +0200

    Handle custom value reset from flow component

--- autoselectcombobox/src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
+++ autoselectcombobox/src/main/java/org/vaadin/addons/autoselectcombobox/AutoSelectComboBox.java
@@ -8,29 +8,43 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import java.util.Collection;
 
 @Tag("vcf-auto-select-combo-box")
-//@NpmPackage(value = "@vaadin-component-factory/vcf-auto-select-combobox", version="24.1.0")
-//@JsModule("@vaadin-component-factory/vcf-auto-select-combobox/src/vcf-auto-select-combobox.js")
-@JsModule("vcf-auto-select-combobox.js")
+@NpmPackage(value = "@vaadin-component-factory/vcf-auto-select-combobox", version="24.1.0")
+@JsModule("@vaadin-component-factory/vcf-auto-select-combobox/src/vcf-auto-select-combobox.js")
+//@JsModule("vcf-auto-select-combobox.js")
 public class AutoSelectComboBox<T> extends ComboBox<T> {
 
     public AutoSelectComboBox() {
         super();
+        init();
     }
 
     public AutoSelectComboBox(int pageSize) {
         super(pageSize);
+        init();
     }
 
     public AutoSelectComboBox(String label, Collection<T> items) {
         super(label, items);
+        init();
     }
 
     public AutoSelectComboBox(String label, T... items) {
         super(label, items);
+        init();
     }
 
     public AutoSelectComboBox(String label) {
         super(label);
+        init();
+    }
+
+    private void init() {
+        addCustomValueSetListener(e -> {
+            // Clear value as custom values are not allowed
+            setValue(null);
+            // Restore the input field text to the custom value
+            getElement().executeJs("this.inputElement.value = $0;", e.getDetail());
+        });
     }
 
     @Override
@@ -40,6 +54,11 @@ public class AutoSelectComboBox<T> extends ComboBox<T> {
 
     @Override
     public void setAllowCustomValue(boolean allowCustomValue) {
+        if (allowCustomValue) {
+            // This is required for addCustomValueSetListener.
+            super.setAllowCustomValue(true);
+            return;
+        }
         throw new UnsupportedOperationException("AutoSelectComboBox does not support custom values. Please remove the call to comboBox.setAllowCustomValue(..)");
     }
 }
